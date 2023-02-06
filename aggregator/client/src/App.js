b51fbef15066dc2faad1ab1d2f6a2e4a04fa9571
Ethan Hulen <ethulen01@gmail.com>
2023-02-06 11:33:51 -0600

    added eslintignor and gitignore

--- aggregator/client/src/App.js
+++ aggregator/client/src/App.js
@@ -14,7 +14,7 @@ import { Masonry } from '@mui/lab';
 
 const App = () => {
 
-	let heights = [32, 45];
+	const heights = [150, 30, 90, 70, 110, 150, 130, 80, 50, 90, 100, 150, 30, 50, 80];
 	let hasAccount;
 
 	return (
