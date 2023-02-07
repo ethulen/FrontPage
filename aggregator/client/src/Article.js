3d7365f201e3bf01ec562ac572d0598a5b241f9e
Ethan Hulen <ethulen01@gmail.com>
2023-02-07 13:45:57 -0600

    added header, categories, and article

---
+++ aggregator/client/src/Article.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Article(props) {
+  return (
+    <div>Article</div>
+  )
+}
+
+Article.propTypes = {}
+
+export default Article
