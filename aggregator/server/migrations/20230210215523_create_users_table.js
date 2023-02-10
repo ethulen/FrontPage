4e51df657601a735fce6bc946286bbbbeabd8089
Ethan Hulen <ethulen01@gmail.com>
2023-02-10 16:26:03 -0600

    added knex

---
+++ aggregator/server/migrations/20230210215523_create_users_table.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('users', function (table) {
+      table.increments('id')
+      table.string('username', 20).unique().notNullable()
+      table.string('password', 255).notNullable()
+      table.text('preferences')
+      table.timestamp('created_at').defaultTo(knex.fn.now())
+      table.timestamp('updated_at').defaultTo(knex.fn.now())
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema
+    .dropTable('users')
+};
