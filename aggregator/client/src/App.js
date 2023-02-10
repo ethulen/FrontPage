609ec3bc1896ed93b47f86b45101107ff9d54533
Ethan Hulen <ethulen01@gmail.com>
2023-02-10 11:36:34 -0600

    base program ui

--- aggregator/client/src/App.js
+++ aggregator/client/src/App.js
@@ -1,43 +1,20 @@
 import React from "react";
-import RegistrationForm from "./RegistrationForm";
-import LoginForm from "./LoginForm";
 import Feed from "./Feed"
 import Header from "./Header"
-import {
-	Paper,
-	Accordion,
-	AccordionSummary,
-	AccordionDetails,
-} from "@mui/material";
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Masonry } from '@mui/lab';
 
 const App = () => {
 
 	const heights = [150, 30, 90, 70, 110, 150, 130, 80, 50, 90, 100, 150, 30, 50, 80];
-	let hasAccount;
+
 
 	return (
 		<div className="home">
-			<Header/>
-			{/* <div>{hasAccount ? <RegistrationForm /> : <LoginForm />}</div> */}
+			<Header />
 			<form className="home__form">
 				<Masonry columns={2} spacing={2}>
 					{heights.map((height, index) => (
-						<Paper key={index}>
-							<Accordion sx={{ minHeight: height }}>
-								<AccordionSummary
-									expandIcon={<ExpandMoreIcon />}
-								>
-								</AccordionSummary>
-								<AccordionDetails>
-									<div>
-										<Feed/>
-									</div>
-								</AccordionDetails>
-							</Accordion>
-						</Paper>
-						
+						<Feed />
 					))}
 				</Masonry>
 			</form>
--- aggregator/client/src/Feed.js
+++ aggregator/client/src/Feed.js
@@ -1,8 +1,9 @@
 import React from "react";
 import axios from 'axios';
+import {Paper} from '@mui/material'
 
-class Feed extends React.Component{
-	constructor(props){
+class Feed extends React.Component {
+    constructor(props) {
         super(props)
         this.state = {
             headlinesNews: [],
@@ -10,70 +11,70 @@ class Feed extends React.Component{
             errors: null
         };
     }
-	getHeadlines(sources) {
+    getHeadlines(sources) {
         // Axios fetches headlines
-        axios.get('https://newsapi.org/v2/everything',{
-            params: {domains: sources, apiKey: 'e188a3e6d6c64590be570b46271bd205'}
+        axios.get('https://newsapi.org/v2/everything', {
+            params: { domains: sources, apiKey: 'e188a3e6d6c64590be570b46271bd205' }
         })
-          // Once a response is obtained, map the API endpoints to props
-          .then(response =>
-            response.data.articles.map(news => ({
-              title: `${news.title}`,
-              description: `${news.description}`,
-              author: `${news.author}`,
-              newsurl: `${news.url}`,
-              url: `${news.urlToImage}`
-            }))
-          )
-          // Change the loading state to display the data
-          .then(headlinesNews => {
-            this.setState({
-              headlinesNews,
-              isLoading: false
-            });
-          })
-          // Use the `.catch()` method since axios is promise-based
-          .catch(error => this.setState({ error, isLoading: false }));
+            // Once a response is obtained, map the API endpoints to props
+            .then(response =>
+                response.data.articles.map(news => ({
+                    title: `${news.title}`,
+                    description: `${news.description}`,
+                    author: `${news.author}`,
+                    newsurl: `${news.url}`,
+                    url: `${news.urlToImage}`
+                }))
+            )
+            // Change the loading state to display the data
+            .then(headlinesNews => {
+                this.setState({
+                    headlinesNews,
+                    isLoading: false
+                });
+            })
+            // Use the `.catch()` method since axios is promise-based
+            .catch(error => this.setState({ error, isLoading: false }));
     }
 
     componentDidMount() {
         this.getHeadlines('reuters.com')
     }
 
-    render(){
+    render() {
         const { isLoading, headlinesNews } = this.state;
         return (
             <React.Fragment>
-            <div>
                 {!isLoading ? (
-                headlinesNews.map(news => {
-                    const { title, description, author, newsurl, url } = news;
-                    return (
-                    <div className="collumn" key={title}>
-                        <div className="head">
-                            <span className="headline hl3">
-                                {title}
-                            </span>
-                            <p>
-                                <span className="headline hl4">
-                                    {author}
-                                </span>
-                            </p>
-                            <figure className="figure">
-								<img className="media" src={url} alt="" />
-						    </figure>
-                            <p>
-                                {description}<br />
-                            </p>
-                            <a href={newsurl} target="_blank" rel="noopener noreferrer">Read full news</a>
-                        </div>
-                    </div>
-                    );
-                })
+                    headlinesNews.map(news => {
+                        const { title, description, author, newsurl, url } = news;
+                        return (
+                            <div className="column" key={title}>
+                                <div className="head">
+                                    <Paper>
+                                        <span className="headline hl3">
+                                            {title}
+                                        </span>
+                                        <p>
+                                            <span className="headline hl4">
+                                                {author}
+                                            </span>
+                                        </p>
+                                        <figure className="figure">
+                                            <img className="media" src={url} alt="" />
+                                        </figure>
+                                        <p>
+                                            {description}<br />
+                                        </p>
+                                        <a href={newsurl} target="_blank" rel="noopener noreferrer">Read full article</a>
+                                    </Paper>
+                                </div>
+                            </div>
+                        );
+                    })
                 ) : (
-                <p>Loading...</p>
+                    <p>Loading...</p>
                 )}
-            </div>
             </React.Fragment>
         );
     }
