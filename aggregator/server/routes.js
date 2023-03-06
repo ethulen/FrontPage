ec3b26c4b2ec1c45af7ba2e711608448d8e5d336
Ethan Hulen <ethulen01@gmail.com>
2023-03-06 15:13:52 -0600

    Removed node_modules folder

--- aggregator/server/routes.js
+++ aggregator/server/routes.js
@@ -2,10 +2,16 @@ const knexDB = require('./knex')
 
 const setUpRoutes = (app) => {
     // POST method route
-    app.post('/register', (req, res) => {
+    app.post('/register', async (req, res) => {
+        val = await knexDB('users').insert({username: req.body.name, email: req.body.email, password: req.body.password})
         res.send('POST request to the homepage')
-        knexDB('users').insert({username: req.body.name, email: req.body.email, password: req.body.password})
     })
+    // GET method route
+    // app.get('/login', (req, res) => {
+    //     knexDB.select('username').from('users')
+    //     knexDB.select('password').from('users')
+    //     res.send('GET request to the homepage')
+    // })
 }
 
 exports.setUpRoutes = setUpRoutes;
