989980bfe354dba1524db46172d4668a90b827bd
Ethan Hulen <ethulen01@gmail.com>
2023-04-10 14:44:54 -0500

    props error

---
+++ aggregator/client/src/Home.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react"
+import Feed from "./Feed"
+import { Masonry } from '@mui/lab';
+
+const Home = () => {
+	const [loggedInAccount, setLoggedInAccount] = useState(0);
+	return (
+		<form className="home__form">
+			<Masonry columns={2} spacing={2}>
+				<Feed loggedInAccount={loggedInAccount}/>
+			</Masonry>
+		</form>)
+}
+export default Home;
